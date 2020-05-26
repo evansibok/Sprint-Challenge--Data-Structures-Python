fed8b9c65d0d4fec498f8ca2b7d1a8da7a76571d
Evans Ibok <evansibok@gmail.com>
2020-05-26 22:20:08 +0100

    complete reverse.py program

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -47,4 +47,22 @@ class LinkedList:
 
     def reverse_list(self, node, prev):
         # You must use recursion for this solution
-        pass
+
+        # 1 -> 2 -> 3 -> 4
+        # 4 -> 3 -> 2 -> 1
+        # If node is empty or node's next is empty
+        if node is None or node.get_next() is None:
+            # return the node
+            # --> if node is None, it will return None
+            # --> if node's next is None, it will return the current node
+            return node
+        # Set the head node to be the recursive call of moving through the nodes
+        self.head = self.reverse_list(node.get_next(), node)
+        # Set the current next of the current node's next element to the current node
+        # Same as pointing the current node's next element's next to the previous node
+        node.get_next().set_next(node)
+        # Point the current node's next to None
+        node.next = None
+        # Return the head node which will be the result of the
+        # recursive call stack (the last held head node)
+        return self.head
